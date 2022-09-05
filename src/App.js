c874cace7e0a449963fdd5a1ea68aa3ab41cde44
joaquin <villanuevajoaquin94@gmail.comn>
2022-09-05 17:18:19 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import './App.css';
-import { GoogleLogin } from 'react-google-login';
+import React from "react";
+import "./App.css";
+import { GoogleLogin } from "@react-oauth/google";
 
 function App() {
-  const responseGoogle = (response) => {
-    console.log(response);
-  }
-  return (
-    <div className="App">
-      <header className="App-header">
-        Login with Google Sign-In
-        <GoogleLogin
-          clientId={process.env.REACT_APP_WEB_CLIENT_ID || ''}
-          buttonText="Login"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={'single_host_origin'}
-        />
-      </header>
-    </div>
-  );
+	const responseGoogle = (response) => {
+		console.log(response);
+	};
+	return (
+		<div className="App">
+			<header className="App-header">
+				Login with Google Sign-In
+				<GoogleLogin onSuccess={responseGoogle} onError={responseGoogle} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<GoogleOAuthProvider clientId={process.env.REACT_APP_WEB_CLIENT_ID}>
+			<App />
+		</GoogleOAuthProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
