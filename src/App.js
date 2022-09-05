7e8deb6c22547676f9283786e1ca7535153855df
Franco Martucci <franco.martucci.21@gmail.com>
2022-09-05 18:56:10 -0300

    Add server login after Google login

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import "./App.css";
 import { GoogleLogin } from "@react-oauth/google";
 
 function App() {
-	const responseGoogle = (response) => {
-		console.log(response);
-	};
-	return (
-		<div className="App">
-			<header className="App-header">
-				Login with Google Sign-In
-				<GoogleLogin onSuccess={responseGoogle} onError={responseGoogle} />
-			</header>
-		</div>
-	);
+  const responseGoogle = async (response) => {
+    console.log("Google response:", response);
+    try {
+      const serverResponse = await fetch(
+        "http://localhost:80/api/v1/auth/google",
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+          body: JSON.stringify({ id_token: response.credential }),
+        }
+      );
+      console.log("Respuesta del servidor", await serverResponse.json());
+    } catch (e) {
+      console.log("Hubo un error al ingresar (SERVER_ERROR)");
+    }
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        Login with Google Sign-In
+        <GoogleLogin onSuccess={responseGoogle} onError={responseGoogle} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
